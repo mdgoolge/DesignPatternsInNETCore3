facf775e18ce824335782b24ae535c4f95312815
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-01-19 11:13:29 +0800

    z

---
+++ PART II Creational Patterns/CHAPTER 6 Singleton/CHAPTER6Singleton/CHAPTER6Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CHAPTER6Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
